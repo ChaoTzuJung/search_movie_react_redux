ac77ca573fe7c2608d942180bbfa1773528e02ae
Alan Chao <s110319022@stu.ntue.edu.tw>
2018-07-26 12:44:06 +0800

    add redux template for default

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux';
+import promiseMiddleware from 'redux-promise';
+
+import reducers from './reducers'
 import Routes from './routes'
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware)(createStore)
 
 const App = () => {
     return(
+    <Provider store={createStoreWithMiddleware(reducers)}>
         <BrowserRouter>
             <Routes/>
         </BrowserRouter>
+    </Provider>
     )
 }
 
